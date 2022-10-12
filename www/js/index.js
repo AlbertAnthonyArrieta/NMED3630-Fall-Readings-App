35b621f063bbb4504534ac767bcb0183ac789097
Arrieta <albert.arrieta@uleth.ca>
2022-10-12 11:46:24 -0600

    added pages

--- www/js/index.js
+++ www/js/index.js
@@ -2,5 +2,27 @@ var app = new Framework7({
     // App root element
     el: '#app',
     // ... other parameters
+    routes: [
+        {
+            path: '/',
+            url: 'index.html',
+        },
+        {
+            path: '/podcasts/',
+            url: 'pages/podcasts.html',
+        },
+        {
+            path: '/movies/',
+            url: 'pages/movies.html',
+        },
+        {
+            path: '/books/',
+            url: 'pages/books.html',
+        },
+        {
+            path: '/music/',
+            url: 'pages/music.html',
+        },
+    ],
   });
 var mainView = app.views.create('.view-main')
